1948bc6f6feff7548591e3f160cc5f6be67a7071
Dhruva Bisht <57747815+dhruvabisht@users.noreply.github.com>
2022-01-12 19:42:30 +0530

    Create 8_n_even.cc

---
+++ Recursion/8_n_even.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void nat(int n, int i)
+{
+    if(i>n)
+    {
+        if(n/2 == 0)
+            cout << n;
+    }
+    else
+    {
+        cout<< i <<' ';
+        nat(n, i+2);
+    }
+}
+
+int main()
+{
+    nat(10,0);
+    return 0;
+}
