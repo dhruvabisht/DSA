3f60b00b4ca9ad8469305a93bf9f53cdecd240f3
Dhruva Bisht <57747815+dhruvabisht@users.noreply.github.com>
2022-01-12 19:18:43 +0530

    Create 2_factorial.cc

---
+++ Recursion/2_factorial.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int fac(int n)
+{
+    if(n==1)
+        return 1;
+    else
+    {
+        return (n* fac(n-1));
+    }
+}
+
+int main()
+{
+    cout<<fac(5);
+    return 0;
+}
